86270b1c377299812b405e82e2b84f184c6f3709
Simon Inazuma <yoshimune2017@gmail.com>
2024-01-20 11:57:30 +0100

    db migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'movies/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get 'movies', to: 'movies#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
